3923d34ee493940c50b938a8e6b5febe95e2297e
Andrew Huang <ahuang@anaconda.com>
2026-01-29 16:35:05 -0800

    Add all

--- template/src/{{ project_slug_snake_case }}/main.py
+++ template/src/{{ project_slug_snake_case }}/main.py
@@ -1,15 +1,26 @@
-"""Main module."""
+"""Main module for the Panel application."""
 
 import panel as pn
 
 
-def create_app():
-    """Create the main Panel application."""
+def create_app() -> pn.Row:
+    """Create the main Panel application.
+
+    Returns
+    -------
+    pn.Row
+        A Panel Row containing an interactive slider and its squared value.
+
+    Examples
+    --------
+    >>> app = create_app()
+    >>> app.servable()
+    """
     pn.extension()
 
     x_slider = pn.widgets.IntSlider(name="x", start=0, end=100)
 
-    def apply_square(x):
+    def apply_square(x: int) -> str:
         return f"{x} squared is {x**2}"
 
     return pn.Row(x_slider, pn.bind(apply_square, x_slider))
