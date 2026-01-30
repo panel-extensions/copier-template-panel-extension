1ac6057447d9cbcdaf2a7c5e3bd5bca68c3cc7e8
Andrew Huang <ahuang@anaconda.com>
2026-01-29 16:24:15 -0800

    add api

--- template/src/{{ project_slug_snake_case }}/main.py
+++ template/src/{{ project_slug_snake_case }}/main.py
@@ -1,1 +1,19 @@
 """Main module."""
+
+import panel as pn
+
+
+def create_app():
+    """Create the main Panel application."""
+    pn.extension()
+
+    x_slider = pn.widgets.IntSlider(name="x", start=0, end=100)
+
+    def apply_square(x):
+        return f"{x} squared is {x**2}"
+
+    return pn.Row(x_slider, pn.bind(apply_square, x_slider))
+
+
+if __name__ == "__main__":
+    create_app().servable()
